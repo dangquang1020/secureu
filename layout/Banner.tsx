5380cfa152609546245a08340198bb39b8ab550b
Quang Tran <dangquang1020@gmail.com>
2021-09-13 19:14:06 +0700

    Update responsive, add content wrapper maxwidth

--- layout/Banner.tsx
+++ layout/Banner.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
-import { Button } from '../components/button/Button';
 import styles from '../styles/modules/Banner.module.scss';
-import bannerBg from '../public/images/banner-bg.png';
-import logo from '../public/images/logo-white-trans.png';
+import { Button } from '../components/button/Button';
+import { ContentWrapper } from '../layout/ContentWrapper';
 import { SectionTitle } from '../components/text/SectionTitle';
 import { SectionDescription } from '../components/text/SectionDescription';
+
+import bannerBg from '../public/images/banner-bg.png';
+import logo from '../public/images/logo-white-trans.png';
 
 const Banner = () => {
   const title1 = "Personal safety on demand."
@@ -13,18 +15,21 @@ const Banner = () => {
 
   return (
     <div className={styles.container}>
-      <div className="-z-1 absolute w-full h-full top-0 left-0">
-        <Image src={bannerBg} layout="fill" objectFit="cover" objectPosition={'75% top'} />
+      <div className={`-z-1 absolute w-full h-full top-0 left-0 ${styles.background}`}>
+        <Image src={bannerBg} layout="fill" objectFit="cover" />
       </div>
 
-      <div className={styles.content}>
-        <Image src={logo} layout='intrinsic' width={123} height={48} />
-        <SectionTitle title={title1} className={`mt-8 ${styles.title}`} />
+      <ContentWrapper>
+        <div className={styles.logo}>
+          <Image src={logo} layout='intrinsic' />
+        </div>
+
+        <SectionTitle title={title1} className={`mt-8 lg:mt-20 ${styles.title}`} />
         <SectionTitle title={title2} className={`mb-5 ${styles.title}`} />
         <SectionDescription description={description} className={styles.description} />
 
-        <Button className="btn-danger mt-10">Learn More</Button>
-      </div>
+        <Button className="btn-danger mt-10 lg:mt-14">Learn More</Button>
+      </ContentWrapper>
     </div>
   )
 }
