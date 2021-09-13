230d5761cddbd57be38cfe16a09a583a0500e8e7
Truong Nguyen <truong.nguyenvan@hdwebsoft.co>
2021-09-13 19:45:02 +0700

    update footer

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -8,8 +8,8 @@ import GGPlay from "../assets/images/gg-play.png";
 
 const Footer = () => (
   <Section>
-    <div className="flex bg-dark px-3 md:px-0 md:py-32 flex-col-reverse md:flex-row items-center">
-      <div className="flex flex-col md:px-32 md:items-start items-center my-16 md:my-0">
+    <div className="flex bg-dark px-3 md:px-0 md:py-32 flex-col-reverse md:flex-row justify-center md:items-center">
+      <div className="flex flex-col md:px-16 lg:px-32 md:items-start items-center my-10 md:my-0">
         <Image src={Logo} alt="Logo" width={172} height={66} />
         <p className="text-white text-sm md:inline-grid">
           <span>© Copyright 2021 SecureU.</span>
@@ -17,7 +17,7 @@ const Footer = () => (
         </p>
       </div>
 
-      <div className="flex items-center text-white max-w-25">
+      <div className="flex items-center text-white md:max-w-25 md:mx-0 mx-auto">
         <div className="flex flex-col mr-8">
           <span className="text-13/16 font-black opacity-35">ABOUT</span>
           <p className="text-mercury text-xs max-w-16">
@@ -37,7 +37,7 @@ const Footer = () => (
         />
       </div>
 
-      <div className="flex md:justify-start justify-between md:flex-col md:px-32 my-16 md:my-0">
+      <div className="flex md:justify-start justify-between md:flex-col md:px-16 lg:px-32 my-10 md:my-0">
         <Image src={GGPlay} alt="Logo" width={172} height={66} />
         <Image src={GGPlay} alt="Logo" width={172} height={66} />
       </div>
